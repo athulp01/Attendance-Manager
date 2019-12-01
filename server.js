c84a3aee79895d25fea5c2d4ec1ef69dbfab38de
Athul P <athul929@gmail.com>
2019-12-01 20:56:47 +0530

    add templating and a cleanup

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require("express")
 var bodyParser = require('body-parser')
-var path = require('path')
 var authenication = require('./routes/authroutes')
+var ejs = require('ejs')
+
 
 var app = express()
 app.use(bodyParser.urlencoded({extended:false}))
@@ -12,6 +13,20 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.set('view engine', 'ejs')
+
+
+app.get('/home', (req, res)=>{
+    let courses = [
+        {name: "Data structures and algorithms",
+         num:   8},
+        {name: "Electronics Circuits 2",
+         num:   5},
+        {name: "Economics",
+         num:   10}
+    ]
+    res.render('home', {user:"Athul", courses:courses})
+})
 app.post('/reg', authenication.register)
 
 app.listen(process.env.PORT||5000)
